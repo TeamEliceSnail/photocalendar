8be3d387ad217fc27666ccd994d4d5c66eca5d25
Seongeuniii <kse8538@naver.com>
2022-04-29 18:29:27 +0900

    clean all

--- src/common/components/navigation/components/navFormStyle.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export default styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    width: 200px;
-    height: 50%;
-`;
