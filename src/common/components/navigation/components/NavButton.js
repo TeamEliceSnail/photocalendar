9d4dc0df93396c1d5ed9f6fe63d3098cd32ce02e
Circlewee <pnj1118@naver.com>
2022-04-29 15:19:37 +0900

    파일 대소문자 구분 설정 완료

---
+++ src/common/components/navigation/components/NavButton.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import buttonImg from '../../../img/calendar.png';
+import Wrapper from './NavButtonStyle';
+
+const NavButton = ({ title }) => {
+    return (
+        <Wrapper>
+            <div id="nav-button-icon">
+                <img src={buttonImg} alt="p" />
+            </div>
+            <div id="nav-button-title">{title}</div>
+        </Wrapper>
+    );
+};
+
+export default NavButton;
