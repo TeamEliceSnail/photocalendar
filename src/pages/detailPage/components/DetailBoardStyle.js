f4e58cee15de36d4bead156a64459d9f832ca705
jinwoo <wwo1278@gmail.com>
2022-04-30 14:53:19 +0900

    proptypes 설치 및 슬라이드 공통 컴포넌트 구성

--- src/pages/detailPage/components/DetailBoardStyle.js
+++ src/pages/detailPage/components/DetailBoardStyle.js
@@ -4,27 +4,29 @@ export default styled.div`
     /* background-color: gray; */
     display: flex;
     flex-direction: column;
-
+    width: 60vh;
     justify-content: center;
     align-items: center;
     height: 100%;
+    flex-shrink: 0;
     & > #title {
-        margin: 5vh;
-        background-color: gray;
-        width: 90vh;
-        font-size: 50px;
-        box-shadow: 2px 2px;
+        /* background-color: gray; */
+        /* width: 90vh; */
+        width: 100%;
+        margin-left: 7vh;
+        font-size: 3rem;
+        /* box-shadow: 2px 2px; */
     }
 
     & > #content {
         margin: 3vh;
         font-size: 25px;
-        background-color: gray;
-        height: 60vh;
-        width: 60vh;
+        /* background-color: gray; */
+        height: 55vh;
+        width: 52vh;
         word-break: break-all;
-        box-shadow: 2px 2px;
-        padding: 30px;
-        border-radius: 30px;
+        /* box-shadow: 2px 2px; */
+        /* padding: 30px; */
+        /* border-radius: 30px; */
     }
 `;
