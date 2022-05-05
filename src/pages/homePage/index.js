85b31731baef93bb4ac890137acd706be30c2fa3
Circlewee <pnj1118@naver.com>
2022-05-05 15:18:12 +0900

    PHOT-23-메인화면-구조-변경

--- src/pages/homePage/index.js
+++ src/pages/homePage/index.js
@@ -1,7 +1,6 @@
 import CalendarHeader from './components/CalendarHeader';
 import CalendarMain from './components/CalendarMain';
 import { HomePageContainer } from './style';
-import { Route, Link } from 'react-router-dom';
 
 const HomePage = () => {
     return (
---
+++ src/recoil/index.js
@@ -0,0 +1,5 @@
+import createBoardState from './createBoardState';
+import currentDateState from './currentDateState';
+import modalState from './modalState';
+
+export { createBoardState, currentDateState, modalState };
