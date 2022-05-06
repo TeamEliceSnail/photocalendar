a43a1b752ad97d1259e99bb32c03bc51a49a3adf
master_jiho <aksjajsxm123@gmail.com>
2022-05-06 18:36:53 +0900

    중간 체크포인트

--- src/router/ctrl.js
+++ src/router/ctrl.js
@@ -16,23 +16,7 @@ const output={
     profile: (req,res)=>{
         res.send("/profile page");
     },
-    uploadImg: (req,res)=>{
-        res.send(`<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title></title>
-    </head>
-    <body>
-        <form action="/sendImg" method="post" enctype="multipart/form-data">
-            <input type="file" name='image' placeholder="Select file"/>
-            <br/>
-            <button>Upload</button>
-        </form>
-    </body>
-    </html>`);
+    
     },
 }
 
