6dff12ab5463e1c880513f48f34df7de9ed9af25
jinwoo <wwo1278@gmail.com>
2022-05-18 03:00:05 +0900

    api 리팩토링 및 버그 수정

---
+++ src/pages/detailPage/api/index.js
@@ -0,0 +1,13 @@
+import { getDetailAllBoard } from './getDetailAllBoard';
+import { postImg } from './postImg';
+import { deleteDetailBoard } from './deleteDetailBoard';
+import { postDetailBoard } from './postDetailBoard';
+import { updateDetailBoard } from './updateDetailBoard';
+
+export {
+    getDetailAllBoard,
+    postImg,
+    deleteDetailBoard,
+    postDetailBoard,
+    updateDetailBoard,
+};
