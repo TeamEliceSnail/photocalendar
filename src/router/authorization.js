8b019f91178828901f813e1575ad811c76d97b59
jiho kim <sokojiho2@gmail.com>
2022-05-19 10:26:23 +0900

    status 수정

--- src/router/authorization.js
+++ src/router/authorization.js
@@ -5,20 +5,19 @@ require('dotenv').config();
 const verifyToken = (req, res, next) => {
     const clientToken = req.cookies.user;
     if (!clientToken) {
-        res.send('http://localhost:3000/login');
+        return res.status(401).json({ error: 'token empty' });
     } else {
         try {
             const decoded = jwt.verify(clientToken, YOUR_SECRET_KEY);
             if (decoded) {
                 res.locals.userId = decoded.user_id;
+                res.status(200);
                 next();
             } else {
-                res.send('http://localhost:3000/login');
                 res.status(401).json({ error: 'unauthorized' });
             }
         } catch (err) {
-            console.log(err);
-            res.send('http://localhost:3000/login');
+            res.status(401).json({ error: err });
         }
     }
 };
